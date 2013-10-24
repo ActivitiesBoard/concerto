2d8f5c9a041992d1b5001b41e71c5e202cee619f
Edward <tedgarb2@gmail.com>
2013-10-23 21:37:27 -0400

    user model changes for shib autocreation

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,11 +65,10 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_shibboleth_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    user = User.where(:email => request.env['REMOTE_USER']).first
     unless user
-      user = User.create(provider:auth.provider,
-                         uid:auth.uid,
-                         password:Devise.friendly_token[0,20])
+      user = User.create(email: request.env['REMOTE_USER'],
+                         password: Devise.friendly_token[0,20])
     end
     user
   end
